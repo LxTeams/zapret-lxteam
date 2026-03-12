c3117c7e4f10f919f1386687d17d42c092958264
LxTeams <powerguitarmusic@gmail.com>
2026-03-12 08:38:22 +0300

    Update general (ALT4).bat

--- general (ALT4).bat
+++ general (ALT4).bat
@@ -39,7 +39,7 @@ rem call service.bat check_updates
 rem call service.bat load_game_filter
 echo:
 
-:: Задаём GameFilter если не задан
+
 if "%GameFilter%"=="" set "GameFilter=12"
 
 set "BIN=%~dp0bin\"
@@ -61,3 +61,4 @@ echo Zapret запущен в фоновом режиме
 echo.
 
 pause
+
